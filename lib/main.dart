6ccd3be1317f76b44a8e15f4a92094066fbca154
55nknown <55922348+55nknown@users.noreply.github.com>
2022-08-06 15:56:22 +0200

    auth test

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,51 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-void main() => runApp(const MyApp());
+import 'package:clipboard/clipboard.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:uni_links/uni_links.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  final box = await Hive.openBox("user");
+
+  runApp(MyApp(box));
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp(this.box, {super.key});
+
+  final Box box;
 
   @override
   Widget build(BuildContext context) {
+    final user = box.get("access_token");
+
+    if (user == null) {
+      uriLinkStream.listen((event) {
+        if (event != null && event.queryParameters["access_token"] != null) {
+          box.put("access_token", event.queryParameters["access_token"]);
+          box.put("refresh_token", event.queryParameters["refresh_token"]);
+        }
+      }, onError: (err) {
+        log("[ERROR] UriLinkStream: $err");
+      });
+
+      launchUrl(Uri.parse("https://api.tear.one/oauth/discord/redirect"), mode: LaunchMode.externalApplication);
+    }
+
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: TextButton(
+            child: Text('Hello World ${box.get("access_token")}'),
+            onPressed: () => FlutterClipboard.copy(box.get("access_token")),
+          ),
         ),
       ),
     );

1dd23b9a571e57c535464e5036b966715c0563b3
ezyyeah <52110827+ezyyeah@users.noreply.github.com>
2022-08-09 22:55:46 +0200

    open settings with profile button

--- lib/ui/mobile/common/profile_button.dart
+++ lib/ui/mobile/common/profile_button.dart
@@ -1,6 +1,9 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tearmusic/providers/user_provider.dart';
+import 'package:tearmusic/ui/mobile/pages/settings/settings_page.dart';
 
 class ProfileButton extends StatelessWidget {
   const ProfileButton({Key? key}) : super(key: key);
@@ -9,11 +12,29 @@ class ProfileButton extends StatelessWidget {
   Widget build(BuildContext context) {
     final avatar = context.select<UserProvider, String>((user) => user.avatar);
 
-    return ClipOval(
-      child: SizedBox(
-        width: 36.0,
-        height: 36.0,
-        child: avatar != "" ? Image.network(avatar) : null,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          PageRouteBuilder(
+            pageBuilder: (context, primaryAnimation, secondaryAnimation) {
+              return FadeThroughTransition(
+                fillColor: Colors.transparent,
+                animation: primaryAnimation,
+                secondaryAnimation: secondaryAnimation,
+                child: const SettingsPage(),
+              );
+            },
+            transitionDuration: const Duration(milliseconds: 500),
+            reverseTransitionDuration: const Duration(milliseconds: 500),
+          ),
+        );
+      },
+      child: ClipOval(
+        child: SizedBox(
+          width: 36.0,
+          height: 36.0,
+          child: avatar != "" ? Image.network(avatar) : null,
+        ),
       ),
     );
   }

e83a761ddaaf5956169dfd1a79c74267279abf0a
55nknown <55922348+55nknown@users.noreply.github.com>
2022-08-19 00:17:50 +0200

    currently playing track indicator

--- lib/ui/mobile/common/tiles/track_tile.dart
+++ lib/ui/mobile/common/tiles/track_tile.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -53,83 +54,124 @@ class TrackTile extends StatelessWidget {
           },
         ),
       ],
-      child: Material(
-        type: MaterialType.transparency,
-        child: ListTile(
-          leading: leadingTrackNumber
-              ? SizedBox(
-                  width: 42,
-                  height: 42,
-                  child: Center(
-                    child: Text(
-                      track.trackNumber.toString(),
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.secondary,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 16.0,
-                      ),
-                    ),
-                  ),
-                )
-              : SizedBox(
-                  width: 42,
-                  height: 42,
-                  child: track.album != null && track.album!.images != null ? CachedImage(track.album!.images!, size: const Size(64, 64)) : null,
-                ),
-          title: Text(
-            track.name,
-            maxLines: 2,
-            softWrap: true,
-            overflow: TextOverflow.ellipsis,
-          ),
-          subtitle: Row(
-            children: [
-              if (track.explicit)
-                Container(
-                  margin: const EdgeInsets.only(right: 6.0),
-                  height: 14,
-                  width: 14,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(2.0),
-                    color: Theme.of(context).colorScheme.onSecondaryContainer,
-                  ),
-                  child: Center(
-                    child: Text(
-                      "E",
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.secondaryContainer,
-                        fontSize: 12.0,
-                        height: -0.05,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              Expanded(
-                child: Text(
-                  track.artistsLabel,
-                  maxLines: 1,
-                  softWrap: false,
-                  overflow: TextOverflow.ellipsis,
+      child: Selector<CurrentMusicProvider, MusicTrack?>(
+        selector: (_, p) => p.playing,
+        builder: (context, value, child) {
+          Widget leading;
+
+          if (leadingTrackNumber) {
+            leading = SizedBox(
+              width: 42,
+              height: 42,
+              child: Center(
+                child: PageTransitionSwitcher(
+                  duration: const Duration(seconds: 1),
+                  transitionBuilder: (child, primaryAnimation, secondaryAnimation) {
+                    return FadeThroughTransition(
+                      fillColor: Colors.transparent,
+                      animation: primaryAnimation,
+                      secondaryAnimation: secondaryAnimation,
+                      child: child,
+                    );
+                  },
+                  child: track == value
+                      ? Icon(
+                          Icons.volume_up,
+                          key: const Key("playing"),
+                          color: Theme.of(context).colorScheme.onSecondaryContainer,
+                        )
+                      : Text(
+                          track.trackNumber.toString(),
+                          key: const Key("track"),
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.secondary,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16.0,
+                          ),
+                        ),
                 ),
               ),
-            ],
-          ),
-          trailing: trailingDuration ? Text(track.duration.shortFormat()) : null,
-          visualDensity: VisualDensity.compact,
-          onTap: () {
-            FocusScope.of(context).requestFocus(FocusNode());
-            final currentMusic = context.read<CurrentMusicProvider>();
-            if (track.album?.images != null) {
-              CachedImage(track.album!.images!).getImage(const Size(64, 64)).then((value) {
-                final colors = generateColorPalette(value);
-                final theme = context.read<ThemeProvider>();
-                if (theme.key != colors[1]) theme.setThemeKey(colors[1]);
-                currentMusic.playTrack(track);
-              });
-            }
-          },
-        ),
+            );
+          } else {
+            leading = SizedBox(
+              width: 42,
+              height: 42,
+              child: Stack(
+                children: [
+                  if (track.album != null && track.album!.images != null) CachedImage(track.album!.images!, size: const Size(64, 64)),
+                  AnimatedOpacity(
+                    duration: const Duration(milliseconds: 500),
+                    opacity: track == value ? 1 : 0,
+                    child: Container(
+                      color: Colors.black.withOpacity(.5),
+                      child: Center(child: Icon(Icons.volume_up, color: Theme.of(context).colorScheme.onSecondaryContainer)),
+                    ),
+                  )
+                ],
+              ),
+            );
+          }
+
+          return Material(
+            type: MaterialType.transparency,
+            child: ListTile(
+              leading: leading,
+              title: Text(
+                track.name,
+                maxLines: 2,
+                softWrap: true,
+                overflow: TextOverflow.ellipsis,
+              ),
+              subtitle: Row(
+                children: [
+                  if (track.explicit)
+                    Container(
+                      margin: const EdgeInsets.only(right: 6.0),
+                      height: 14,
+                      width: 14,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(2.0),
+                        color: Theme.of(context).colorScheme.onSecondaryContainer,
+                      ),
+                      child: Center(
+                        child: Text(
+                          "E",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.secondaryContainer,
+                            fontSize: 12.0,
+                            height: -0.05,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  Expanded(
+                    child: Text(
+                      track.artistsLabel,
+                      maxLines: 1,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+              trailing: trailingDuration ? Text(track.duration.shortFormat()) : null,
+              visualDensity: VisualDensity.compact,
+              onTap: () {
+                FocusScope.of(context).requestFocus(FocusNode());
+                final currentMusic = context.read<CurrentMusicProvider>();
+                if (track.album?.images != null) {
+                  CachedImage(track.album!.images!).getImage(const Size(64, 64)).then((value) {
+                    final colors = generateColorPalette(value);
+                    final theme = context.read<ThemeProvider>();
+                    if (theme.key != colors[1]) theme.setThemeKey(colors[1]);
+                    currentMusic.playTrack(track);
+                  });
+                }
+              },
+            ),
+          );
+        },
       ),
     );
   }

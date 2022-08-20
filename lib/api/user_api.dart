0d7f38006e6d62b2178626ec15945ec91cd78533
ezyyeah <52110827+ezyyeah@users.noreply.github.com>
2022-08-20 21:16:35 +0200

    user music library

--- lib/api/user_api.dart
+++ lib/api/user_api.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:tearmusic/api/base_api.dart';
 import 'package:http/http.dart' as http;
 import 'package:tearmusic/exceptionts.dart';
+import 'package:tearmusic/models/library.dart';
+import 'package:tearmusic/models/music/track.dart';
 import 'package:tearmusic/models/user_info.dart';
 
 class UserApi {
@@ -10,16 +13,53 @@ class UserApi {
 
   BaseApi base;
 
+  void _reschk(http.Response res, String cause) {
+    cause = "UserApi.$cause";
+    if (res.statusCode == 401) {
+      throw AuthException(cause);
+    }
+    if (res.statusCode == 404) {
+      throw NotFoundException(cause);
+    }
+    if (res.statusCode != 200) {
+      log("Unknown Request: ${res.statusCode}");
+      throw UnknownRequestException(cause);
+    }
+  }
+
   Future<UserInfo> getInfo() async {
     final res = await http.get(
       Uri.parse("${BaseApi.url}/user/info"),
       headers: {"authorization": await base.getToken()},
     );
 
-    if (res.statusCode != 200) {
-      throw AuthException("UserApi.getInfo");
-    }
+    _reschk(res, "getInfo");
 
-    return UserInfo.fromJson(jsonDecode(res.body));
+    return UserInfo.decode(jsonDecode(res.body));
+  }
+
+  Future<UserLibrary> getLibrary() async {
+    final res = await http.get(
+      Uri.parse("${BaseApi.url}/user/music-library"),
+      headers: {"authorization": await base.getToken()},
+    );
+
+    _reschk(res, "getLibrary");
+
+    return UserLibrary.decode(jsonDecode(res.body));
+  }
+
+  Future<void> putLibrary(String id, LibraryType type) async {
+    final res = await http.put(Uri.parse("${BaseApi.url}/user/music-library"),
+        headers: {"authorization": await base.getToken(), "content-type": "application/json"}, body: jsonEncode({"id": id, "type": type.name}));
+
+    _reschk(res, "putLibrary");
+  }
+
+  Future<void> deleteLibrary(String id, LibraryType type) async {
+    final res = await http.delete(Uri.parse("${BaseApi.url}/user/music-library"),
+        headers: {"authorization": await base.getToken(), "content-type": "application/json"}, body: jsonEncode({"id": id, "type": type.name}));
+
+    _reschk(res, "deleteLibrary");
   }
 }
--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:tearmusic/api/base_api.dart';
 import 'package:tearmusic/api/user_api.dart';
 import 'package:tearmusic/exceptionts.dart';
+import 'package:tearmusic/models/library.dart';
+import 'package:tearmusic/models/model.dart';
 import 'package:tearmusic/providers/music_info_provider.dart';
 
 class UserProvider extends ChangeNotifier {
@@ -15,6 +19,8 @@ class UserProvider extends ChangeNotifier {
     _username = _store.get("username");
     _avatar = _store.get("avatar");
     _id = _store.get("id");
+    final stLibrary = _store.get("library");
+    if(stLibrary != null) _library = UserLibrary.decode(jsonDecode(stLibrary));
 
     String? accessToken = _store.get("access_token");
     String? refreshToken = _store.get("refresh_token");
@@ -38,6 +44,7 @@ class UserProvider extends ChangeNotifier {
   String? _id;
   String? _username;
   String? _avatar;
+  UserLibrary? _library;
   String get username => _username ?? "";
   String get avatar => _avatar ?? "";
 
@@ -48,6 +55,7 @@ class UserProvider extends ChangeNotifier {
     _store.delete("avatar");
     _store.delete("access_token");
     _store.delete("refresh_token");
+    _store.delete("library");
 
     final cacheBox = await Hive.openBox("cached_images");
     cacheBox.clear();
@@ -72,10 +80,12 @@ class UserProvider extends ChangeNotifier {
   Future<void> _getUser() async {
     try {
       final user = await _api.getInfo();
+      final library = await _api.getLibrary();
 
       _username = user.username;
       _avatar = user.avatar;
       _id = user.id;
+      _library = library;
       notifyListeners();
 
       _musicInfoProvider.userId = _id ?? "";
@@ -83,9 +93,85 @@ class UserProvider extends ChangeNotifier {
       _store.put("username", _username);
       _store.put("avatar", _avatar);
       _store.put("id", _id);
+      _store.put("library", jsonEncode(_library!.encode()));
     } on AuthException {
       loggedIn = false;
       notifyListeners();
     }
+  }
+
+  Future<UserLibrary> getLibrary() async {
+    if (_library == null) {
+      final library = await _api.getLibrary();
+      _library = library;
+      _store.put("library", jsonEncode(_library!.encode()));
+    }
+
+    return _library!;
+  }
+
+  Future<void> putLibrary(Model model, LibraryType type) async {
+    await getLibrary();
+
+    final id = model.id;
+
+    switch (type) {
+      case LibraryType.liked_tracks:
+        if (_library!.liked_tracks.contains(id)) return;
+        _library!.liked_tracks.add(id);
+        break;
+      case LibraryType.liked_artists:
+        if (_library!.liked_artists.contains(id)) return;
+        _library!.liked_artists.add(id);
+        break;
+      case LibraryType.liked_albums:
+        if (_library!.liked_albums.contains(id)) return;
+        _library!.liked_albums.add(id);
+        break;
+      case LibraryType.liked_playlists:
+        if (_library!.liked_playlists.contains(id)) return;
+        _library!.liked_playlists.add(id);
+        break;
+      case LibraryType.track_history:
+        _library!.track_history = _library!.track_history
+            .where((item) => item != id)
+            .toList()
+            .sublist((_library!.track_history.length - 49).clamp(0, 49), _library!.track_history.length - 1)
+          ..add(id);
+        break;
+    }
+
+    await _api.putLibrary(id, type);
+    _store.put("library", jsonEncode(_library!.encode()));
+  }
+
+  Future<void> deleteLibrary(Model model, LibraryType type) async {
+    await getLibrary();
+
+    final id = model.id;
+
+    switch (type) {
+      case LibraryType.liked_tracks:
+        if (!_library!.liked_tracks.contains(id)) return;
+        _library!.liked_tracks.remove(id);
+        break;
+      case LibraryType.liked_artists:
+        if (!_library!.liked_artists.contains(id)) return;
+        _library!.liked_artists.remove(id);
+        break;
+      case LibraryType.liked_albums:
+        if (!_library!.liked_albums.contains(id)) return;
+        _library!.liked_albums.remove(id);
+        break;
+      case LibraryType.liked_playlists:
+        if (!_library!.liked_playlists.contains(id)) return;
+        _library!.liked_playlists.remove(id);
+        break;
+      case LibraryType.track_history:
+        break;
+    }
+
+    await _api.deleteLibrary(id, type);
+    _store.put("library", jsonEncode(_library!.encode()));
   }
 }

6ee08a583e7130b234e88059add914c5a74ab887
ezyyeah <52110827+ezyyeah@users.noreply.github.com>
2022-08-22 03:33:39 +0200

    library track history

--- lib/models/library.dart
+++ lib/models/library.dart
@@ -5,7 +5,7 @@ class UserLibrary {
   List<String> liked_artists = [];
   List<String> liked_albums = [];
   List<String> liked_playlists = [];
-  List<String> track_history = [];
+  List<UserTrackHistory> track_history = [];
 
   UserLibrary({
     required this.liked_tracks,
@@ -21,17 +21,50 @@ class UserLibrary {
       liked_artists: (json["liked_artists"] as List).cast<String>(),
       liked_albums: (json["liked_albums"] as List).cast<String>(),
       liked_playlists: (json["liked_playlists"] as List).cast<String>(),
-      track_history: (json["track_history"] as List).cast<String>(),
+      track_history: (json["track_history"] as List).map((e) => UserTrackHistory.decode(e)).toList(),
+    );
+  }
+
+  Map encode() {
+    //print("encoding ${track_history.map((e) => e.encode())}");
+    return {
+      "liked_tracks": liked_tracks,
+      "liked_artists": liked_artists,
+      "liked_albums": liked_albums,
+      "liked_playlists": liked_playlists,
+      "track_history": track_history.map((e) => e.encode()).toList(),
+    };
+  }
+}
+
+enum LibraryType { liked_tracks, liked_artists, liked_albums, liked_playlists, track_history }
+
+class UserTrackHistory {
+  int date;
+  String track_id;
+  String? from_id;
+  String? from_type;
+
+  UserTrackHistory({
+    required this.date,
+    required this.track_id,
+    this.from_id,
+    this.from_type,
+  });
+
+  factory UserTrackHistory.decode(Map json) {
+    return UserTrackHistory(
+      date: json["date"],
+      track_id: json["track_id"],
+      from_id: json["from_id"],
+      from_type: json["from_type"],
     );
   }
 
   Map encode() => {
-        "liked_tracks": liked_tracks,
-        "liked_artists": liked_artists,
-        "liked_albums": liked_albums,
-        "liked_playlists": liked_playlists,
-        "track_history": track_history,
+        "date": date,
+        "track_id": track_id,
+        "from_id": from_id,
+        "from_type": from_type,
       };
 }
-
-enum LibraryType {liked_tracks, liked_artists, liked_albums, liked_playlists, track_history}

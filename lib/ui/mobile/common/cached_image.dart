cc4be6c007d50bf268e93e39501291207f541bd9
55nknown <annon@annon.me>
2023-01-05 15:31:04 +0100

    beautiful custom image loading

--- lib/ui/mobile/common/cached_image.dart
+++ lib/ui/mobile/common/cached_image.dart
@@ -35,44 +35,44 @@ class CachedImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-      return ClipRRect(
-        borderRadius: BorderRadius.circular(borderRadius),
-        child: SizedBox(
-          width: constraints.maxWidth,
-          height: constraints.maxHeight,
-          child: PageTransitionSwitcher(
-            transitionBuilder: (child, primaryAnimation, secondaryAnimation) {
-              return FadeThroughTransition(
-                fillColor: Colors.transparent,
-                animation: primaryAnimation,
-                secondaryAnimation: secondaryAnimation,
-                child: child,
-              );
-            },
-            child: CachedNetworkImage(
-              imageUrl: images.forSize(size ?? (Size(constraints.maxWidth, constraints.maxHeight))),
-              placeholder: (context, url) => SizedBox(
+      return FutureBuilder<Uint8List>(
+          future: getImage(size ?? (Size(constraints.maxWidth, constraints.maxHeight))),
+          builder: (context, snapshot) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(borderRadius),
+              child: SizedBox(
                 width: constraints.maxWidth,
                 height: constraints.maxHeight,
-                child: Card(
-                  elevation: 0,
-                  margin: EdgeInsets.zero,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderRadius)),
-                  color: Theme.of(context).colorScheme.secondaryContainer,
-                  child: Icon(
-                    CupertinoIcons.music_note,
-                    size: sqrt(constraints.maxWidth * constraints.maxHeight) / 2,
-                    color: Theme.of(context).colorScheme.secondary,
-                  ),
+                child: PageTransitionSwitcher(
+                  transitionBuilder: (child, primaryAnimation, secondaryAnimation) {
+                    return FadeThroughTransition(
+                      fillColor: Colors.transparent,
+                      animation: primaryAnimation,
+                      secondaryAnimation: secondaryAnimation,
+                      child: child,
+                    );
+                  },
+                  child: snapshot.hasData
+                      ? Image.memory(snapshot.data!)
+                      : SizedBox(
+                          width: constraints.maxWidth,
+                          height: constraints.maxHeight,
+                          child: Card(
+                            elevation: 0,
+                            margin: EdgeInsets.zero,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderRadius)),
+                            color: Theme.of(context).colorScheme.secondaryContainer,
+                            child: Icon(
+                              CupertinoIcons.music_note,
+                              size: sqrt(constraints.maxWidth * constraints.maxHeight) / 2,
+                              color: Theme.of(context).colorScheme.secondary,
+                            ),
+                          ),
+                        ),
                 ),
               ),
-              fadeInDuration: const Duration(milliseconds: 50),
-              fadeOutDuration: const Duration(milliseconds: 50),
-              placeholderFadeInDuration: const Duration(milliseconds: 50),
-            ),
-          ),
-        ),
-      );
+            );
+          });
     });
   }
 }
